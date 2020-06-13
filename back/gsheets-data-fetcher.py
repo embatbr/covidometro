57c0f5e707493ba47a6f9a8b47e6119abfde7a14
Eduardo Tenório <embatbr@gmail.com>
2020-06-13 04:25:04 -0300

    Fetching data and storing in a CSV file.

--- back/gsheets-data-fetcher.py
+++ back/gsheets-data-fetcher.py
@@ -1,31 +1,73 @@
 # -*- coding: utf-8 -*-
 
+import sys
+
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 import pandas as pd
 
+
+DATA_DIR = 'data'
 
 SCOPE = [
     'https://spreadsheets.google.com/feeds',
     'https://www.googleapis.com/auth/drive'
 ]
 
-CREDENTIALS = ServiceAccountCredentials.from_json_keyfile_name(
+DEFAULT_CREDENTIALS = ServiceAccountCredentials.from_json_keyfile_name(
     'credentials/covidometro-info-78d047b1cb0b.json',
     SCOPE
 )
 
+SOURCE_GSHEETS_KEY = '1MWQE3s4ef6dxJosyqvsFaV4fDyElxnBUB6gMGvs3rEc'
 
-client = gspread.authorize(CREDENTIALS)
-# print(dir(client))
+NUM_COLUMNS = 8
+DEFAULT_HEADER = [
+    'Estado', 'Total de Casos', 'Suspeitos', 'Curados', 'Óbitos', 'Testes',
+    'Novos Casos','Novos Óbitos'
+]
 
-worksheet = client.open_by_key('1MWQE3s4ef6dxJosyqvsFaV4fDyElxnBUB6gMGvs3rEc').sheet1
-# worksheet = client.open_by_url(
-#     'https://docs.google.com/spreadsheets/d/1MWQE3s4ef6dxJosyqvsFaV4fDyElxnBUB6gMGvs3rEc/edit?usp=sharing'
-# ).sheet1
 
-data = worksheet.get_all_values()
-headers = data.pop(0)
+def read_values(gsheet_key, credentials):
+    client = gspread.authorize(credentials)
 
-df = pd.DataFrame(data, columns=headers)
-print(df.head())
+    sheet = client.open_by_key(gsheet_key)
+    worksheet = sheet.sheet1
+
+    cells = worksheet.range('B3:I31')
+    values = list()
+    cur_index = -1
+    count = 0
+
+    for cell in cells:
+        if count % NUM_COLUMNS == 0:
+            values.append(list())
+            cur_index = cur_index + 1
+
+        value = cell.value
+        values[cur_index].append(value)
+
+        count = count + 1
+
+    return values
+
+
+def read_values_as_dataframe(gsheet_key, credentials):
+    data = read_values(gsheet_key, credentials)
+    data.pop(0) # popping header
+
+    data[0][0] = 'BRASIL'
+
+    return pd.DataFrame(data, columns=DEFAULT_HEADER)
+
+
+def generate_data_file(filename):
+    filepath = '{}/{}.csv'.format(DATA_DIR, filename)
+
+    dataframe = read_values_as_dataframe(SOURCE_GSHEETS_KEY, DEFAULT_CREDENTIALS)
+    dataframe.to_csv(filepath, index=False)
+
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    generate_data_file(filename)
